671bb007e602cadb889a8e48e190d5da52f6c3ad
Daniel Gamboa Estrada <dgamboaestrada@gmail.com>
2023-07-24 12:29:43 -0600

    Change the JQuery script to javascript vanilla copy-jira-ticket-id.user.js

--- jira/copy-jira-ticket-id.user.js
+++ jira/copy-jira-ticket-id.user.js
@@ -5,8 +5,8 @@
 // @description  Copy Jira Id task
 // @author       Daniel Gamboa Estrada
 // @match        https://*.atlassian.net/browse/*
+// @match        https://*.atlassian.net/jira/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=atlassian.net
-// @require      https://code.jquery.com/jquery-3.7.0.slim.min.js
 // @grant        none
 // ==/UserScript==
 
@@ -14,6 +14,22 @@
     'use strict';
 
     // Your code here...
-    const ticketID = jQuery('a[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"]').find('span').text();
-    jQuery(".css-1src8bx.er2v6i55").append("<div><a href=\"javascript:navigator.clipboard.writeText('" + ticketID + "');\">Copy Ticket ID</a></div>");
+    setInterval(addCopyTextID, 5000);
 })();
+
+function addCopyTextID() {
+    var copyTextId = document.getElementById("copy-text-id");
+    var issueLink = document.querySelector('a[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"]');
+
+    if (copyTextId == null && issueLink != null) {
+        var ticketID = document.querySelector('a[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"] span').textContent;
+        var element = document.querySelector('.css-1src8bx.er2v6i55');
+        var div = document.createElement('div');
+        var a = document.createElement('a');
+        a.id = 'copy-text-id';
+        a.href = 'javascript:navigator.clipboard.writeText("'+ ticketID +'");';
+        a.textContent = 'Copy Ticket ID';
+        div.appendChild(a);
+        element.appendChild(div);
+    }
+}
